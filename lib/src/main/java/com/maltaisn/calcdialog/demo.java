e0c710b07fdf5cfb990ab70b41a3a430dcbf9cdf
Suzon70 <masumsuzon10@gmail.com>
2021-11-23 15:52:47 +0600

    demo.java file added

---
+++ lib/src/main/java/com/maltaisn/calcdialog/demo.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public  class demo{
+    int num =4;
+    char chr;
+
+    public demo(int num, char chr) {
+        this.num = num;
+        this.chr = chr;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public void setChr(char chr) {
+        this.chr = chr;
+    }
+}
